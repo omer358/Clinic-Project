28b867fa9e79666c6c37434c936dbf6cef5b942f
Omer Maki <46290816+omer358@users.noreply.github.com>
2020-01-11 22:49:13 -1200

    inital commit

---
+++ src/clinicproject/ClinicProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clinicproject;
+
+/**
+ *
+ * @author O.M.O
+ */
+public class ClinicProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginScreen.main(args);
+    }
+    
+}
